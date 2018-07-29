6a71968f9ac57296ec482e439b400ee888f300b9
GabbyCuascota <gabriela.cuascota@hotmail.com>
2018-07-29 17:38:17 -0500

    afinando pantallas

--- ClinicaDentalVistas/src/app/sc-cirugia/sc-cirugia.component.ts
+++ ClinicaDentalVistas/src/app/sc-cirugia/sc-cirugia.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-sc-cirugia',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScCirugiaComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
+
